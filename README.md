eb7288262810df880b2884b40534e88a62f69fcb
aternyx <aterny@shuriken.pm>
2024-04-14 19:02:59 +0300

    update

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 [![MyBB](https://raw.github.com/mybb/mybb/feature/images/logo.png "MyBB")](http://www.mybb.com "MyBB")
 ==========
 ## Limba română ptr. MyBB
+Atentie: extensia nu este terminata momentan
 ### Compatibilitate
 Folositi acesta DOAR PENTRU VERSIUNEA MYBB 1.8 SI MAI NOU.
 
 ## MyBB Romanian Language Pack
+Warnung: pack is not finished at the moment
 ### Compatibility
 Use this ONLY FOR MYBB VERSION 1.8 AND NEWER.
 
---
+++ upload/romanian.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * MyBB 1.8 Romanian Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ * Copyright 2024 aterny, Toate Drepturile Rezervate
+ * 
+ */
+
+// The friendly name of the language
+$langinfo['name'] = "Romanian";
+
+// The author of the language
+$langinfo['author'] = "aterny";
+
+// The language authors website
+$langinfo['website'] = "https://zero.24-7.ro/";
+
+// The documentation site link
+$langinfo['docs_link'] = "https://github.com/aternyx/mybb-romanian";
+
+// The documentation FAQs site link
+$langinfo['common_issues_link'] = "https://github.com/aternyx/mybb-romanian";
+
+// The support website link
+$langinfo['support_link'] = "https://github.com/aternyx/mybb-romanian";
+
+// The additional website name
+//$langinfo['additional_name'] = " ";
+
+// Compatible version of MyBB
+$langinfo['version'] = "1837";
+
+// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['admin'] = 1;
+
+// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
+$langinfo['rtl'] = 0;
+
+// Sets the lang in the <html> on all pages
+$langinfo['htmllang'] = "ro";
+
+// Sets the character set, blank uses the default.
+$langinfo['charset'] = "UTF-8";
